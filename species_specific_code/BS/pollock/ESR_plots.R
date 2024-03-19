74df1fa21006da4d002a34e40f9f41ccc4dd06cd
Sophia Wassermann <hi@sowasser.com>
2024-03-19 16:29:35 -0700

    Fixed transformation of COG easting/northing (UTM) to lat/long for ESR

--- species_specific_code/BS/pollock/ESR_plots.R
+++ species_specific_code/BS/pollock/ESR_plots.R
@@ -11,6 +11,7 @@ library(reshape2)
 library(viridis)
 library(sf)
 library(rnaturalearth)
+library(akgfmaps)
 
 # Set ggplot theme
 devtools::install_github("seananderson/ggsidekick")
@@ -29,7 +30,7 @@ cog <- data.frame(results$Range$COG_Table)
 cog$Year <- as.numeric(cog$Year)
 cog$COG_hat <- as.numeric(cog$COG_hat)
 cog$SE <- as.numeric(cog$SE)
-# write.csv(cog, file = here(workDir, "results", "COG.csv"), row.names = FALSE)
+# write.csv(cog, file = here(workDir, "results", "ESR products", "COG.csv"), row.names = FALSE)
 
 # Original plot - UTM
 cog$m[cog$m == 1] <- "Easting"
@@ -39,6 +40,7 @@ cog_plot <- ggplot(cog %>% filter(Year != 2020), aes(x = Year, y = COG_hat)) +
   geom_line(alpha = 0.4) +
   geom_pointrange(aes(ymin = (COG_hat - SE), ymax = (COG_hat + SE))) +
   ylab("Center of Gravity") +
+  ylim(0, NA) +
   facet_wrap(~ m, ncol = 1, scales = "free_y")
 cog_plot
 
@@ -51,25 +53,26 @@ cog_north <- cog %>% filter(m == "Northing")
 cog_north <- cog_north[, 2:3]
 colnames(cog_north)[2] <- "Northing"
 
-# Convert to lat & lon with sf
-# TODO: figure out correct CRS
-cog_latlon <- cbind.data.frame(cog_east, Northing = cog_north[, 2]) %>%
-  st_as_sf(coords = c("Easting", "Northing"), crs = 4326) %>%  # convert to an sf object
-  st_transform(4326) %>%  # transform or convert coordinates of sf
-  st_coordinates() %>%  # retrieve coordinates in matrix form
-  as_tibble() 
+# Do conversion using akgfmaps package
+cog2 <- cbind.data.frame(X = cog_east[, 2], Y = cog_north[, 2])
+# CRS information for VAST outputs here: 
+# https://github.com/James-Thorson-NOAA/FishStatsUtils/blob/main/R/project_coordinates.R
+cog_latlon <- transform_data_frame_crs(cog2, 
+                                       coords = c("X", "Y"), 
+                                       in.crs = "+proj=utm +datum=WGS84 +units=km +zone=2",
+                                       out.crs = "+proj=longlat +datum=WGS84")
 cog_latlon$Year <- cog_east$Year
 
 world <- ne_countries(scale = "medium", returnclass = "sf")
 sf_use_s2(FALSE)  # turn off spherical geometry
-
-ggplot(data = world) +
+cog_map <- ggplot(data = world) +
   geom_sf() +
   geom_point(data = cog_latlon,
              aes(x = X, y = Y, color = Year)) +
-  coord_sf(xlim = c(-179, -157), ylim = c(54, 66), expand = FALSE) +
+  coord_sf(xlim = c(-179, -157), ylim = c(54, 65), expand = FALSE) +
   scale_color_viridis(option = "plasma", discrete = FALSE, end = 0.9) +
   xlab(" ") + ylab(" ")
+cog_map
 
 # Effective area occupied -----------------------------------------------------
 options(scipen = 999)
@@ -79,8 +82,8 @@ report <- TMB::summary.sdreport(full_fit$parameter_estimates$SD)
 area <- report[which(rownames(report) == "log_effective_area_ctl"), c('Estimate', 'Std. Error')]
 Year <- sort(unique(full_fit$year_labels))
 area <- as.data.frame(cbind(area, Year))
-area <- ln_km2[which(ln_km2$Year %in% unique(full_fit$data_frame$t_i)), ]
-# write.csv(ln_km2, file = here(workDir, "results", "ln_effective_area.csv"), 
+area <- area[which(area$Year %in% unique(full_fit$data_frame$t_i)), ]
+# write.csv(area, file = here(workDir, "results", "ESR products", "ln_effective_area.csv"),
 #           row.names = FALSE)
 
 area$Region <- c(rep("Both", length(1982:this_year) - 1), 
@@ -105,7 +108,9 @@ area_plot <- ggplot(area %>% filter(Year != 2020), aes(x = Year, y = Estimate, c
 area_plot
 
 # Save plots ------------------------------------------------------------------
-# ggsave(cog_plot, filename = here("VAST_results", "2023_pollock_COG.png"),
-#        width = 150, height = 180, unit = "mm", dpi = 300)
-# ggsave(area_plot, filename = here("VAST_results", "2023_pollock_area.png"),
-#        width = 150, height = 100, unit = "mm", dpi = 300)
+ggsave(cog_plot, filename = here(workDir, "results", "ESR products", "2023_pollock_COG.png"),
+       width = 150, height = 180, unit = "mm", dpi = 300)
+ggsave(cog_map, filename = here(workDir, "results", "ESR products", "2023_pollock_COG_map.png"),
+       width = 110, height = 90, unit = "mm", dpi = 300)
+ggsave(area_plot, filename = here(workDir, "results", "ESR products", "2023_pollock_area.png"),
+       width = 150, height = 100, unit = "mm", dpi = 300)
