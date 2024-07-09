d19c03630e4397dd5c9d36dc31956718b68a3b5e
Sophia Wassermann NOAA <sophia.wassermann@noaa.gov>
2024-07-09 14:04:18 -0700

    Updated comparison plots for tinyVAST (all years, tweedie) age comps

--- species_specific_code/BS/pollock/Comparison_plots.R
+++ species_specific_code/BS/pollock/Comparison_plots.R
@@ -11,7 +11,7 @@ library(reshape2)
 library(viridis)
 
 # Set ggplot theme
-# devtools::install_github("seananderson/ggsidekick")
+devtools::install_github("seananderson/ggsidekick")
 library(ggsidekick)
 theme_set(theme_sleek())
 
@@ -91,7 +91,7 @@ old_props <- read.csv(here(workDir, "results", "Comps", "proportions.csv"))[, -1
 new_props <- read.csv(here(workDir, "results", "tinyVAST_props.csv"))
 
 # Update old_props to match tinyVAST test output
-tiny_years <- c(2010:2019, 2021:2023)
+tiny_years <- c(1980:2019, 2021:2023)
 old_props <- old_props %>% filter(Year %in% tiny_years & Region == "EBS")
 
 ## Combine age comp models into one plot --------------------------------------
@@ -111,12 +111,12 @@ compare_props <- function(props, names, last_year) {
     scale_fill_viridis(discrete = TRUE, option = "plasma", end = 0.9) +
     scale_x_discrete(breaks = c(1, 5, 10, 15)) +
     ylab("Proportion-at-age") +
-    facet_wrap(~ Year, ncol = 3) 
+    facet_wrap(~ Year, ncol = 6) 
   return(plot)
 }
 
 all_props <- compare_props(props = list(new_props, old_props),
-                           names = c("tinyVAST", "2023 production"))
+                           names = c("tinyVAST (Tweedie)", "2023 production"))
 all_props
 
 # Plot difference between two models ------------------------------------------
@@ -149,13 +149,13 @@ comp_difference <- function(new, old, names, save_results = FALSE) {
     scale_fill_manual(values = c("cornflowerblue", "darkred")) +
     scale_x_discrete(breaks = c(1, 5, 10, 15)) +
     ylab(label) +
-    facet_wrap(~ Year, ncol = 3) 
+    facet_wrap(~ Year, ncol = 6) 
   
   return(plot)
 }
 
 comp_diff <- comp_difference(new = new_props, old = old_props,
-                             names = c("tinyVAST", "2023 production"),
+                             names = c("tinyVAST (Tweedie)", "2023 production"),
                              save_results = FALSE)
 comp_diff
 
@@ -164,7 +164,7 @@ ggsave(comp_index, filename = here(workDir, "results", save_dir, "index_comparis
        width=130, height=160, units="mm", dpi=300)
 ggsave(index_diff, filename = here(workDir, "results", save_dir, "index_difference.png"),
        width=130, height=180, units="mm", dpi=300)
-ggsave(all_props, filename = here(workDir, "results", "age_comp_compare_tinyVAST.png"),
-       width=130, height=130, units="mm", dpi=300)
-ggsave(comp_diff, filename = here(workDir, "results", "age_comp_diff_tinyVAST.png"),
-       width=100, height=130, units="mm", dpi=300)
+ggsave(all_props, filename = here(workDir, "results", "age_comp_compare_tinyTweedie.png"),
+       width=200, height=180, units="mm", dpi=300)
+ggsave(comp_diff, filename = here(workDir, "results", "age_comp_diff_tinyTweedie.png"),
+       width=200, height=200, units="mm", dpi=300)
