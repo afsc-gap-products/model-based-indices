2a1dc888f8a01cd2fb0098ee00b0b602ab249296
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2025-01-14 08:45:45 -0600

    temp changing gitignore to upload new goa grid

--- extrapolation_grids/alternative_goa_grids/goa_2025_interpolation_grid.R
+++ extrapolation_grids/alternative_goa_grids/goa_2025_interpolation_grid.R
@@ -52,22 +52,22 @@ write.csv(x = goa_grid_df,
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ##   Compare with prior interpolation grid
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-goa_base <- akgfmaps::get_base_layers(select.region = "goa", 
-                                      set.crs = "+proj=utm +zone=5 +units=km")
-## Import prior interpolation grid
-prior_grid <- 
-  read.csv(file = "extrapolation_grids/GOAThorsonGrid_Less700m.csv") |>
-  terra::vect(geom = c("Longitude", "Latitude"), 
-              crs = "+proj=longlat +datum=WGS84") |>
-  terra::project("+proj=utm +zone=5 +units=km")
-
-## Compare total survey areas
-sum(terra::expanse(x = goa_grid) / 1e6) ## 300459.4 km2
-sum(prior_grid$Shape_Area / 1e6) ## 304953.1 km2
-# 1.473571% reduction in total area
-
-## Plot differences
-plot(goa_footprint_2025, mar = c(0,0,0,0))
-plot(prior_grid, add = TRUE, cex = 0.2, pch = 15, col = "red")
-plot(goa_grid, add = TRUE, col = "black", border = TRUE)
-plot(st_geometry(goa_base$akland), add = TRUE, col = "tan")
+# goa_base <- akgfmaps::get_base_layers(select.region = "goa", 
+#                                       set.crs = "+proj=utm +zone=5 +units=km")
+# ## Import prior interpolation grid
+# prior_grid <- 
+#   read.csv(file = "extrapolation_grids/GOAThorsonGrid_Less700m.csv") |>
+#   terra::vect(geom = c("Longitude", "Latitude"), 
+#               crs = "+proj=longlat +datum=WGS84") |>
+#   terra::project("+proj=utm +zone=5 +units=km")
+# 
+# ## Compare total survey areas
+# sum(terra::expanse(x = goa_grid) / 1e6) ## 300459.4 km2
+# sum(prior_grid$Shape_Area / 1e6) ## 304953.1 km2
+# # 1.473571% reduction in total area
+# 
+# ## Plot differences
+# plot(goa_footprint_2025, mar = c(0,0,0,0))
+# plot(prior_grid, add = TRUE, cex = 0.2, pch = 15, col = "red")
+# plot(goa_grid, add = TRUE, col = "black", border = TRUE)
+# plot(st_geometry(goa_base$akland), add = TRUE, col = "tan")
