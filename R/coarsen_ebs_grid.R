c27ff8f33c8bc3bc344759a4b9d12511d724fac0
Lewis Barnett <lewis.barnett@noaa.gov>
2025-01-31 15:53:20 -0800

    revise grids to match stratum areas

--- R/coarsen_ebs_grid.R
+++ R/coarsen_ebs_grid.R
@@ -1,79 +1,81 @@
-# coarsen Bering prediction grid for faster index model computation
+## Title: coarsen Bering prediction grids for index models ----
+# date: Jan 31 2025
+# author: Lewis Barnett, with code from Sean Rohan
 
 library(akgfmaps)
 
-# Load survey area polygon
-bs <- akgfmaps::get_base_layers("ebs", set.crs = "EPSG:32602")
+# Setup grid using dimensions used to create 2022 updated grid
+bbox = c(xmin = -2846314.206900,
+        ymin = 251495.775400,
+        xmax = 2128157.793100,
+        ymax = 2859111.775400)
 
-ggplot() +
-  geom_sf(data = bs$survey.area,
-          fill = NA,
-          mapping = aes(color = "Survey area"))
+# EBS alone ----
+ebs_layers <- akgfmaps::get_base_layers(select.region = "ebs", set.crs = "EPSG:3338")
 
-# create a coarsened grid using the raster package
-resolution_original <- 3704
-resolution <- resolution_original * 6 # define what factor to coarsen by
+# original resolution is 2x2nm (3704, 3704), coarsened to 12x12nm here
+ebs_grid <- akgfmaps:::make_2d_grid(obj = dplyr::filter(ebs_layers$survey.strata, 
+                                                        SURVEY_DEFINITION_ID == 98),
+                                    resolution = c(3704, 3704)*6, 
+                                    bbox = bbox,
+                                    output_type = "point",
+                                    include_tile_center = TRUE) |>
+  sf::st_transform(crs = "EPSG:32602")
 
-r <- raster::raster(as(bs$survey.area, "Spatial"), resolution = resolution)
-rr <- raster::rasterize(as(bs$survey.area, "Spatial"), r, getCover = TRUE)
+ebs_grid[, c('LON_UTM', "LAT_UTM")] <- sf::st_coordinates(ebs_grid)
 
-grid <- as.data.frame(raster::rasterToPoints(rr))
-grid$area <- grid$layer * resolution * resolution
-grid <- dplyr::filter(grid, area > 0) |>
-  dplyr::mutate(X = x / 1000, Y = y / 1000) 
-grid$area_km2 <- grid$area/1e6
-grid <- dplyr::select(grid, X, Y, area_km2)
+# change units to km 
+ebs_grid <- ebs_grid |>
+  dplyr::mutate(X = LON_UTM / 1000,
+                Y = LAT_UTM / 1000,
+                area_km2 = as.numeric(AREA)/1e6) |> 
+  dplyr::select(X, Y, area_km2) |>
+  as.data.frame()
 
-ggplot(grid, aes(X, Y, colour = area_km2)) +
+ggplot2::ggplot(ebs_grid, aes(X, Y, colour = area_km2)) +
   geom_tile(width = 2, height = 2, fill = NA) +
   scale_colour_viridis_c(direction = -1) +
   geom_point(size = 0.5) +
   coord_fixed()
 
-nrow(grid) # check N cells: results in 1539 for 6x resolution
+write.csv(ebs_grid, file = here::here("extrapolation_grids", "ebs_coarse_grid.csv"))
+
+# NBS alone ----
+nbs_grid <- akgfmaps:::make_2d_grid(obj = dplyr::filter(ebs_layers$survey.strata, 
+                                                        SURVEY_DEFINITION_ID == 143),
+                                    resolution = c(3704, 3704)*6, 
+                                    bbox = bbox,
+                                    output_type = "point",
+                                    include_tile_center = TRUE) |>
+  sf::st_transform(crs = "EPSG:32602")
+
+nbs_grid[, c('LON_UTM', "LAT_UTM")] <- sf::st_coordinates(nbs_grid)
+
+# change units to km 
+nbs_grid <- nbs_grid |>
+  dplyr::mutate(X = LON_UTM / 1000,
+                Y = LAT_UTM / 1000,
+                area_km2 = as.numeric(AREA)/1e6) |> 
+  dplyr::select(X, Y, area_km2) |>
+  as.data.frame()
+
+ggplot2::ggplot(nbs_grid, aes(X, Y, colour = area_km2)) +
+  geom_tile(width = 2, height = 2, fill = NA) +
+  scale_colour_viridis_c(direction = -1) +
+  geom_point(size = 0.5) +
+  coord_fixed()
+
+write.csv(nbs_grid, file = here::here("extrapolation_grids", "nbs_coarse_grid.csv"))
+
+# combine to get full Bering grid ----
+grid <- dplyr::bind_rows(nbs_grid, ebs_grid)
+
+ggplot2::ggplot(grid, aes(X, Y, colour = area_km2)) +
+  geom_tile(width = 2, height = 2, fill = NA) +
+  scale_colour_viridis_c(direction = -1) +
+  geom_point(size = 0.5) +
+  coord_fixed()
+
+nrow(grid) # check N cells: results in 1908 for 12x12nm resolution
 
 write.csv(grid, file = here::here("extrapolation_grids", "bering_coarse_grid.csv"))
-
-# repeat above for EBS alone ----
-
-ebs <- akgfmaps::get_base_layers("sebs", set.crs = "EPSG:32602")
-
-r_ebs <- raster::raster(as(ebs$survey.area, "Spatial"), resolution = resolution)
-rr_ebs <- raster::rasterize(as(ebs$survey.area, "Spatial"), r_ebs, getCover = TRUE)
-
-grid_ebs <- as.data.frame(raster::rasterToPoints(rr_ebs))
-grid_ebs$area <- grid_ebs$layer * resolution * resolution
-grid_ebs <- dplyr::filter(grid_ebs, area > 0) |>
-  dplyr::mutate(X = x / 1000, Y = y / 1000) 
-grid_ebs$area_km2 <- grid_ebs$area/1e6
-grid_ebs <- dplyr::select(grid_ebs, X, Y, area_km2)
-
-ggplot(grid_ebs, aes(X, Y, colour = area_km2)) +
-  geom_tile(width = 2, height = 2, fill = NA) +
-  scale_colour_viridis_c(direction = -1) +
-  geom_point(size = 0.5) +
-  coord_fixed()
-
-write.csv(grid_ebs, file = here::here("extrapolation_grids", "ebs_coarse_grid.csv"))
-
-# repeat above for NBS alone ----
-
-nbs <- akgfmaps::get_base_layers("nbs", set.crs = "EPSG:32602")
-
-r_nbs <- raster::raster(as(nbs$survey.area, "Spatial"), resolution = resolution)
-rr_nbs <- raster::rasterize(as(nbs$survey.area, "Spatial"), r_nbs, getCover = TRUE)
-
-grid_nbs <- as.data.frame(raster::rasterToPoints(rr_nbs))
-grid_nbs$area <- grid_nbs$layer * resolution * resolution
-grid_nbs <- dplyr::filter(grid_nbs, area > 0) |>
-  dplyr::mutate(X = x / 1000, Y = y / 1000) 
-grid_nbs$area_km2 <- grid_nbs$area/1e6
-grid_nbs <- dplyr::select(grid_nbs, X, Y, area_km2)
-
-ggplot(grid_nbs, aes(X, Y, colour = area_km2)) +
-  geom_tile(width = 2, height = 2, fill = NA) +
-  scale_colour_viridis_c(direction = -1) +
-  geom_point(size = 0.5) +
-  coord_fixed()
-
-write.csv(grid_nbs, file = here::here("extrapolation_grids", "nbs_coarse_grid.csv"))
