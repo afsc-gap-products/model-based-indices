4709ffe1e34a4727186c9f3ff514b9a470237740
zoyafuso-NOAA <60302777+zoyafuso-NOAA@users.noreply.github.com>
2025-07-31 11:05:40 -0700

    updated goa interpolation grid to match survey footprint

--- extrapolation_grids/alternative_goa_grids/goa_2025_interpolation_grid.R
+++ extrapolation_grids/alternative_goa_grids/goa_2025_interpolation_grid.R
@@ -13,14 +13,16 @@ library(sf)
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 goa_footprint_2025 <- 
   ## Import goa strata
-  terra::vect(x = paste0("extrapolation_grids/alternative_goa_grids/",
-                         "goa_strata_2025.gpkg"))|> 
-  ## Project to UTM 
-  terra::project("+proj=utm +zone=5 +units=km") |>
+  akgfmaps::get_base_layers(
+    select.region = "goa",
+    design.year = 2025,
+    set.crs = "+proj=utm +zone=5 +units=km"
+  )$survey.strata |>
   ## Remove strata in the 700 - 1000 m depth zone
-  subset(DEPTH_MAX_M != 1000, NSE = TRUE) |>
+  subset(subset = STRATUM < 500) |>
   ## Dissolve boundaries
-  terra::aggregate()
+  terra::vect() |>
+  terra::aggregate() 
 
 goa_grid <- 
   ## Create a rectangular grid that overlaps with the 2025 GOA footprint
@@ -33,7 +35,7 @@ goa_grid <-
 
 goa_grid_df <- data.frame(terra::centroids(x = goa_grid) |> terra::crds()) 
 names(x = goa_grid_df) <- c("X", "Y")
- 
+
 goa_grid_df[, c("lon", "lat")] <- 
   terra::project(x = goa_grid, "+proj=longlat +datum=WGS84") |> 
   terra::centroids() |> 
@@ -44,30 +46,15 @@ goa_grid_df$area_km2 <- terra::expanse(x = goa_grid) / 1e6
 goa_grid_df <- subset(x = goa_grid_df,
                       subset = area_km2 >= 0.001)
 
-write.csv(x = goa_grid_df, 
-        file = paste0("extrapolation_grids/",
-                      "goa_2025_interpolation_grid.csv"), 
-        row.names = F)
+## Check to make sure the total area of the interpolation grid matches the 
+## actual survey footprint.
+sum(goa_grid_df$area_km2) # 301174.5 km2
+expanse(goa_footprint_2025) * 1e-6 # 301174.7 km2
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-##   Compare with prior interpolation grid
+##   Write to file
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# goa_base <- akgfmaps::get_base_layers(select.region = "goa", 
-#                                       set.crs = "+proj=utm +zone=5 +units=km")
-# ## Import prior interpolation grid
-# prior_grid <- 
-#   read.csv(file = "extrapolation_grids/GOAThorsonGrid_Less700m.csv") |>
-#   terra::vect(geom = c("Longitude", "Latitude"), 
-#               crs = "+proj=longlat +datum=WGS84") |>
-#   terra::project("+proj=utm +zone=5 +units=km")
-# 
-# ## Compare total survey areas
-# sum(terra::expanse(x = goa_grid) / 1e6) ## 300459.4 km2
-# sum(prior_grid$Shape_Area / 1e6) ## 304953.1 km2
-# # 1.473571% reduction in total area
-# 
-# ## Plot differences
-# plot(goa_footprint_2025, mar = c(0,0,0,0))
-# plot(prior_grid, add = TRUE, cex = 0.2, pch = 15, col = "red")
-# plot(goa_grid, add = TRUE, col = "black", border = TRUE)
-# plot(st_geometry(goa_base$akland), add = TRUE, col = "tan")
+write.csv(x = goa_grid_df,
+          file = paste0("extrapolation_grids/",
+                        "goa_2025_interpolation_grid.csv"),
+          row.names = F)
